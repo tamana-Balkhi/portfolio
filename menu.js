fef2c654516915bf5305a933cc277bb7c0e77e7b
tamana balkhi <tamana.balkhi1377@gmail.com>
2022-07-20 18:37:13 +0430

    connect mobile navbar to each part of portfolio

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 const mobileHamburger = document.getElementById('hamburger');
 const closeIcon = document.getElementById('cancel');
 const mobileM = document.querySelector('.hide');
+const mobilemenu = document.querySelector('#mobilemenu');
 
 function openMenu() {
   mobileM.style.display = 'block';
@@ -12,3 +13,10 @@ function closeMenu() {
 
 mobileHamburger.addEventListener('click', openMenu);
 closeIcon.addEventListener('click', closeMenu);
+
+mobilemenu.addEventListener('click', () => {
+  mobileM.style.display = 'none';
+  mobileHamburger.style.display = 'block';
+});
+
+
