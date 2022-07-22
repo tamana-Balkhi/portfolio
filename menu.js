1a20b077b33907279fbab990b09f8d901ca3cae5
angeluray <aeucampos@gmail.com>
2022-07-22 04:51:05 -0400

    Fix spacing between keys and values

--- menu.js
+++ menu.js
@@ -142,8 +142,7 @@ const popupwindowArray = [
 
     title: ' Multy Post - Stories 2',
     image: 'images/SnapshootPortfolio.svg',
-    paragraph:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    paragraph:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
     tech1: 'Html',
     tech2: 'Ruby on rails',
     tech3: 'Css',
